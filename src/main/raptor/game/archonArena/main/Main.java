9ae54be081e2e8fcbee15cf90ab84732938ecbf9
raptor0719 <shortman_gamer@hotmail.com>
2022-02-20 19:05:27 -0500

    Begin messing around with the engine to get a feel for things

---
+++ src/main/raptor/game/archonArena/main/Main.java
@@ -0,0 +1,57 @@
+package raptor.game.archonArena.main;
+
+import java.awt.AWTException;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.GridBagLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
+
+import raptor.engine.display.render.JavaAwtRenderer;
+import raptor.engine.display.render.ViewportToLocationTransformer;
+import raptor.engine.game.Game;
+import raptor.engine.ui.input.JavaAwtInputManager;
+import raptor.game.archonArena.menus.MainMenu;
+
+public class Main {
+	public static void main(final String[] args) throws AWTException, InterruptedException {
+		final GraphicsEnvironment graphicsEnvironment = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		final GraphicsDevice[] graphicsDevices = graphicsEnvironment.getScreenDevices();
+
+		final JFrame jframe = new JFrame(graphicsDevices[1].getDefaultConfiguration());
+		jframe.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		jframe.setUndecorated(true);
+		jframe.setVisible(true);
+		jframe.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+		jframe.setLayout(new GridBagLayout());
+
+		final JPanel displayPanel = new JPanel();
+		displayPanel.setPreferredSize(new Dimension(jframe.getWidth(), jframe.getHeight()));
+		jframe.add(displayPanel);
+		displayPanel.setVisible(true);
+		displayPanel.setBackground(Color.BLACK);
+
+		Thread.sleep(250);
+
+		final JavaAwtRenderer renderer = new JavaAwtRenderer((Graphics2D)displayPanel.getGraphics(), displayPanel.getWidth(), displayPanel.getHeight());
+		final ViewportToLocationTransformer viewportToLocationTransformer = new ViewportToLocationTransformer(renderer.getViewport());
+		final JavaAwtInputManager inputManager = new JavaAwtInputManager(viewportToLocationTransformer);
+
+		displayPanel.addMouseListener(inputManager);
+		displayPanel.addKeyListener(inputManager);
+
+		displayPanel.requestFocusInWindow();
+
+		final Game game = new Game(new MainMenu(), renderer, inputManager, inputManager);
+
+		game.start();
+
+		jframe.dispose();
+	}
+}
---
+++ src/main/raptor/game/archonArena/menus/MainMenu.java
@@ -0,0 +1,81 @@
+package raptor.game.archonArena.menus;
+
+import raptor.engine.display.render.BasicColor;
+import raptor.engine.display.render.IGraphics;
+import raptor.engine.game.Game;
+import raptor.engine.game.Level;
+import raptor.engine.ui.UIAnchorPoint;
+import raptor.engine.ui.UIState;
+import raptor.engine.ui.UserInterface;
+import raptor.engine.ui.elements.UIButton;
+import raptor.engine.ui.input.IActionHandler;
+import raptor.engine.ui.input.IInputMap;
+import raptor.engine.ui.input.InputMap;
+import raptor.engine.ui.input.KeyAction;
+import raptor.engine.ui.input.PhysicalInput;
+import raptor.engine.util.geometry.Point;
+
+public class MainMenu extends Level {
+	private final UIState mainMenuState;
+	private UIButton button;
+
+	public MainMenu() {
+		this.mainMenuState = new UIState();
+	}
+
+	@Override
+	public void init() {
+		mainMenuState.addActionHandler("EXIT_GAME", new IActionHandler() {
+			@Override
+			public void handleAction(final int gameMouseX, final int gameMouseY) {
+				Game.exitGame();
+			}
+		});
+
+		final int x = Game.getRenderer().getViewport().getWidth()/2;
+		final int y = Game.getRenderer().getViewport().getHeight()/2;
+
+		button = new UIButton(x, y, UIAnchorPoint.CENTER, 100, 50, 0) {
+			@Override
+			public void draw(final IGraphics graphics) {
+				graphics.drawRectangle(getAbsoluteX(), getAbsoluteY(), getWidth(), getHeight(), new BasicColor(255, 255, 255, 100));
+			}
+
+			@Override
+			public void activate() {
+				Game.exitGame();
+			}
+		};
+
+		mainMenuState.addElement(button);
+
+		final IInputMap inputMap = new InputMap();
+		inputMap.mapInput(PhysicalInput.LEFT_MOUSE, KeyAction.RELEASED, UserInterface.ACTIVATE_BUTTON_ACTION);
+		inputMap.mapInput(PhysicalInput.F_KEY, KeyAction.RELEASED, "EXIT_GAME");
+
+		mainMenuState.setInputMap(inputMap);
+
+		Game.getUserInterface().setState(mainMenuState);
+	}
+
+	@Override
+	public void update() {
+		final Point p = Game.getUserInterface().getMousePosition();
+		button.setX(p.getX());
+		button.setY(p.getY());
+	}
+
+	@Override
+	public void draw(final IGraphics graphics) {
+		final int viewportWidth = Game.getRenderer().getViewport().getWidth();
+		final int viewportHeight = Game.getRenderer().getViewport().getHeight();
+
+		final BasicColor coolPurpleColorMan = new BasicColor(50, 0, 150, 200);
+		final int barWidth = 100;
+
+		graphics.drawRectangle(0, 0, viewportWidth, barWidth, coolPurpleColorMan);
+		graphics.drawRectangle(0, 0, barWidth, viewportHeight, coolPurpleColorMan);
+		graphics.drawRectangle(viewportWidth - barWidth, 0, barWidth, viewportHeight, coolPurpleColorMan);
+		graphics.drawRectangle(0, viewportHeight - barWidth, viewportWidth, barWidth, coolPurpleColorMan);
+	}
+}
