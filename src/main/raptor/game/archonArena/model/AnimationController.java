16a8e874d6259e4134586543c12f9b05ceff13c0
raptor0719 <shortman_gamer@hotmail.com>
2022-02-28 20:38:00 -0500

    Add some basic method signatures for AnimationController

--- src/main/raptor/game/archonArena/model/AnimationController.java
+++ src/main/raptor/game/archonArena/model/AnimationController.java
@@ -1,5 +1,23 @@
 package raptor.game.archonArena.model;
 
 public class AnimationController {
+	public void advanceFrame() {
 
+	}
+
+	public void playAnimation(final String animation) {
+
+	}
+
+	public void loopAnimation(final String animation) {
+
+	}
+
+	public boolean isActivationFrame() {
+
+	}
+
+	public boolean isAnimationFinished() {
+
+	}
 }
