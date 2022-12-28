038f4443d639f721803a67f0a6171e5cdffd7d34
raptor0719 <shortman_gamer@hotmail.com>
2022-12-28 15:31:32 -0500

    Calculate stats and cache intermediate/final results

--- src/main/java/raptor/game/archonArena/unit/stats/UnitStats.java
+++ src/main/java/raptor/game/archonArena/unit/stats/UnitStats.java
@@ -1,6 +1,7 @@
 package raptor.game.archonArena.unit.stats;
 
 import raptor.game.archonArena.unit.stats.blocks.CompositeStatBlock;
+import raptor.game.archonArena.unit.stats.blocks.IStatBlock;
 import raptor.game.archonArena.unit.stats.blocks.StatBlockValueCache;
 import raptor.game.archonArena.unit.stats.blocks.StaticStatBlock;
 
@@ -44,10 +45,86 @@ public class UnitStats {
 		this.extraCache = new StatBlockValueCache(1, base.supportedResources());
 
 		this.finalCache = new StatBlockValueCache(1, base.supportedResources());
-
 	}
 
 	public void calculate() {
+		calculateCompositeResult(baseFactor, baseFactorCache);
+		multiplyStatBlocks(base, baseFactorCache, baseCalculatedCache);
 
+		calculateCompositeResult(bonus, bonusCache);
+		calculateCompositeResult(bonusFactor, bonusFactorCache);
+		multiplyStatBlocks(bonusCache, bonusFactorCache, bonusCalculatedCache);
+
+		addStatBlocks(baseCalculatedCache, bonusCalculatedCache, calculatedCache);
+
+		calculateCompositeResult(calculatedFactor, calculatedFactorCache);
+		multiplyStatBlocks(calculatedCache, calculatedFactorCache, totalCache);
+
+		calculateCompositeResult(extra, extraCache);
+
+		addStatBlocks(totalCache, extraCache, finalCache);
+	}
+
+	/* CALCULATE HELPER METHODS */
+
+	private void calculateCompositeResult(final CompositeStatBlock composite, final StatBlockValueCache cache) {
+		for (final Stat stat : Stat.values())
+			cache.setStatValue(stat, composite.statValue(stat));
+
+		for (final AbilityResource resource : base.supportedResources())
+			for (final AbilityResourceStat stat : AbilityResourceStat.values())
+				cache.setResourceStatValue(resource, stat, composite.resourceStatValue(resource, stat));
+	}
+
+	private void addStatBlocks(final IStatBlock a, final IStatBlock b, final StatBlockValueCache cache) {
+		for (final Stat stat : Stat.values())
+			cache.setStatValue(stat, addStat(stat.getType(), a.statValue(stat), b.statValue(stat)));
+
+		for (final AbilityResource resource : base.supportedResources())
+			for (final AbilityResourceStat stat : AbilityResourceStat.values())
+				cache.setResourceStatValue(resource, stat, addStat(stat.getType(), a.resourceStatValue(resource, stat), b.resourceStatValue(resource, stat)));
+	}
+
+	private void multiplyStatBlocks(final IStatBlock a, final IStatBlock b, final StatBlockValueCache cache) {
+		for (final Stat stat : Stat.values())
+			cache.setStatValue(stat, multiplyStat(stat.getType(), a.statValue(stat), b.statValue(stat)));
+
+		for (final AbilityResource resource : base.supportedResources())
+			for (final AbilityResourceStat stat : AbilityResourceStat.values())
+				cache.setResourceStatValue(resource, stat, multiplyStat(stat.getType(), a.resourceStatValue(resource, stat), b.resourceStatValue(resource, stat)));
+	}
+
+	private Number addStat(final StatType type, final Number a, final Number b) {
+		if (type == StatType.INTEGER)
+			return addInteger(a, b);
+		else if (type == StatType.DOUBLE)
+			return addDouble(a, b);
+		else
+			throw new RuntimeException("Unexpected stat type");
+	}
+
+	private Number multiplyStat(final StatType type, final Number a, final Number b) {
+		if (type == StatType.INTEGER)
+			return multiplyInteger(a, b);
+		else if (type == StatType.DOUBLE)
+			return multiplyDouble(a, b);
+		else
+			throw new RuntimeException("Unexpected stat type");
+	}
+
+	private Number addInteger(final Number a, final Number b) {
+		return a.intValue() + b.intValue();
+	}
+
+	private Number multiplyInteger(final Number a, final Number b) {
+		return a.intValue() * b.intValue();
+	}
+
+	private Number addDouble(final Number a, final Number b) {
+		return a.doubleValue() + b.doubleValue();
+	}
+
+	private Number multiplyDouble(final Number a, final Number b) {
+		return a.doubleValue() * b.doubleValue();
 	}
 }
